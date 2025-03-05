37d0856eda3be221581e083556066817fc990f87
bryanfalk <bryan.falk@nebraska.gov>
2025-03-05 14:04:43 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -138,11 +138,3 @@ async function loadPage() {
 }
 
 loadPage();
-
-const renderer = new marked.Renderer();
-const linkRenderer = renderer.link;
-renderer.link = (href, title, text) => {
-    const html = linkRenderer.call(renderer, href, title, text);
-    return html.replace(/^<a /, '<a target="_blank" rel="nofollow" ');
-};
-const html = marked(markdown, { renderer });
